f4b31e5ad5da7975e7240b00a916bf7ec88fe539
Juxnlx <juanluisbrisquez@gmail.com>
2025-10-15 14:34:19 +0200

    Ejemplo Completo
    ghjgy

---
+++ Ejemplo/Ejemplo/Controllers/FileteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo.Controllers
+{
+    public class FileteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Empanado() { 
+            return View();
+        }
+    }
+}
