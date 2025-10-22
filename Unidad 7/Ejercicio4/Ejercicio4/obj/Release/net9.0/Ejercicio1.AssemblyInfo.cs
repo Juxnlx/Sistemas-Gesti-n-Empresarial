02529673f8ef4a5c2169bb1dc2c1f98ea599f4df
Juxnlx <juanluisbrisquez@gmail.com>
2025-10-22 14:36:56 +0200

    Ejercicio 4 - Completo

--- Unidad 7/Ejercicio4/Ejercicio4/obj/Release/net9.0/Ejercicio1.AssemblyInfo.cs
+++ Unidad 7/Ejercicio4/Ejercicio4/obj/Release/net9.0/Ejercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9438b6d393eaf8061246fc7364704f8edff7af0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483f89d563574ab096c991d808692c12e3be95e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
