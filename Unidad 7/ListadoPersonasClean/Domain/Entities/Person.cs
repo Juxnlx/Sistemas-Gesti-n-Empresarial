52b916d82f4e364af2485cb20c519b03f35fcee5
Juxnlx <juanluisbrisquez@gmail.com>
2025-10-28 18:58:29 +0100

    A medias

--- Unidad 7/ListadoPersonasClean/Domain/Entities/Person.cs
+++ Unidad 7/ListadoPersonasClean/Domain/Entities/Person.cs
@@ -2,8 +2,29 @@ namespace ListadoPersonasClean.Domain.Entities
 {
     public class Person
     {
+        // Propiedades
         public int Id { get; set; }
-        public string Name { get; set; } = "";
+        public string Name { get; set; }
         public int Age { get; set; }
+
+        // Constructor por defecto
+        public Person()
+        {
+        }
+
+        // Constructor con parámetros
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        // Métodos opcionales de la entidad
+        public string GetInfo()
+        {
+            return $"Id: {Id}, Name: {Name}, Age: {Age}";
+        }
+
     }
 }
