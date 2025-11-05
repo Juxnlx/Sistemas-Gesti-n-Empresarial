625b76621ccd637f58a573baf6d15aa4de91a098
Juxnlx <juanluisbrisquez@gmail.com>
2025-11-05 14:36:47 +0100

    completarlo

--- Unidad 7/ListadoPersonasClean/Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
+++ Unidad 7/ListadoPersonasClean/Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8917451836a64890c7167551424c77df7ef81264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb910a56fcd53696a17970ed676bad30dd545a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
