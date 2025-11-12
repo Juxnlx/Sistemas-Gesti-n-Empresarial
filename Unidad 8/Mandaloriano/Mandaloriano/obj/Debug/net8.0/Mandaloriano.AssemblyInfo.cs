4795499567a3289a64acf36e143f1f1f8025dbb5
Juxnlx <juanluisbrisquez@gmail.com>
2025-11-12 14:42:04 +0100

    hecho

--- Unidad 8/Mandaloriano/Mandaloriano/obj/Debug/net8.0/Mandaloriano.AssemblyInfo.cs
+++ Unidad 8/Mandaloriano/Mandaloriano/obj/Debug/net8.0/Mandaloriano.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mandaloriano")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2ec307303a9352aa728b172b03cf60999d5cba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357c858006fcc7d94072f2a5b2c0359b3b9ae593")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mandaloriano")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mandaloriano")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
