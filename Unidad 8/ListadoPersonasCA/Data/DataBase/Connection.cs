9623e98d56911dc9f7ee5cc4abfc0e583346942f
Juxnlx <juanluisbrisquez@gmail.com>
2025-11-19 14:42:16 +0100

    en proceso...

---
+++ Unidad 8/ListadoPersonasCA/Data/DataBase/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DataBase
+{
+    internal class Connection
+    {
+        public static string getConnectionString() {
+           return "server=jlbarrionuevo.database.windows.net;database=PersonasDB;uid=prueba;pwd=.abcd1234!;trustServerCertificate = true;";
+        }
+    }
+}
