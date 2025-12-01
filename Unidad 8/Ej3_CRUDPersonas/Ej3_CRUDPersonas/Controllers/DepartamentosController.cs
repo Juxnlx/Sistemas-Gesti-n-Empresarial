8e0f232d07685281cc9ceb98219131a88159584c
Juxnlx <juanluisbrisquez@gmail.com>
2025-12-01 01:00:36 +0100

    Completo

---
+++ Unidad 8/Ej3_CRUDPersonas/Ej3_CRUDPersonas/Controllers/DepartamentosController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Ej3_CRUDPersonas.Data;
+
+namespace Ej3_CRUDPersonas.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private readonly Ej3_CRUDPersonasContext _context;
+
+        public DepartamentosController(Ej3_CRUDPersonasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Departamentos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamento.ToListAsync());
+        }
+
+        // GET: Departamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nombre")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento.FindAsync(id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nombre")] Departamento departamento)
+        {
+            if (id != departamento.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var departamento = await _context.Departamento.FindAsync(id);
+            if (departamento != null)
+            {
+                _context.Departamento.Remove(departamento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentoExists(int id)
+        {
+            return _context.Departamento.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Unidad 8/Ej3_CRUDPersonas/Ej3_CRUDPersonas/Controllers/PersonasController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Ej3_CRUDPersonas.Data;
+
+namespace Ej3_CRUDPersonas.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private readonly Ej3_CRUDPersonasContext _context;
+
+        public PersonasController(Ej3_CRUDPersonasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Personas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Persona.ToListAsync());
+        }
+
+        // GET: Personas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var persona = await _context.Persona
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return View(persona);
+        }
+
+        // GET: Personas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nombre,Apellidos,Edad,FechaNacimiento,Direccion,Telefono")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(persona);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var persona = await _context.Persona.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+            return View(persona);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nombre,Apellidos,Edad,FechaNacimiento,Direccion,Telefono")] Persona persona)
+        {
+            if (id != persona.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(persona);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonaExists(persona.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var persona = await _context.Persona
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return View(persona);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var persona = await _context.Persona.FindAsync(id);
+            if (persona != null)
+            {
+                _context.Persona.Remove(persona);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonaExists(int id)
+        {
+            return _context.Persona.Any(e => e.ID == id);
+        }
+    }
+}
