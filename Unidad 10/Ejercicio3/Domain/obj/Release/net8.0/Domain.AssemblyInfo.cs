d4ed9c02c9894936476b916841f2196de46deb31
Juxnlx <juanluisbrisquez@gmail.com>
2026-01-22 13:44:53 +0100

    LLORO

--- Unidad 10/Ejercicio3/Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
+++ Unidad 10/Ejercicio3/Domain/obj/Release/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b5602608110e60838acaa3cdd4a946596e9993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6d75dc953f7e6d58d5c70ede6046935ca123b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Unidad 10/Ejercicio3/Ejercicio3/obj/Debug/net8.0/UI.AssemblyInfo.cs
+++ Unidad 10/Ejercicio3/Ejercicio3/obj/Debug/net8.0/UI.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74834437ced2acdcdeb541a9811a8d0065921005")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d190743d6136d0a9c7519b7268cbef0c7ab812e")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6d75dc953f7e6d58d5c70ede6046935ca123b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
